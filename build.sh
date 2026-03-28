e109862573d95cb621795c0beb94190680f815cc
zarra <xsp@xyan.net.cn>
2026-03-28 19:04:38 +0800

    更新 GitHub Actions 工作流，添加构建包类型和发布说明输入，重构构建脚本，删除不再使用的 LLVM 构建脚本

--- build.sh
+++ build.sh
@@ -5,16 +5,19 @@ ROOT=$(cd `dirname $0` && pwd)
 
 # $1 package 
 # $2 version
-# $3 archive dir
-# $4 build dir
+# $3 command
+# $4 archive dir
 # $5 dest dir
+# $6 build dir
+
 
 PACKAGE="${1:-"compiler"}"
 VERSION="${2:-"gcc-11.5.0"}"
 COMMAND="${3:-"build"}"
 ARCHIVE_DIR="${4:-"$ROOT/archive"}"
-BUILD_DIR="${5:-"$ROOT/build"}"
-DEST_DIR="${6:-"$ROOT/dist"}"
+DEST_DIR="${5:-"$ROOT/dist"}"
+BUILD_DIR="${6:-"$ROOT/build"}"
+
 
 mkdir -p $ARCHIVE_DIR
 mkdir -p $BUILD_DIR
